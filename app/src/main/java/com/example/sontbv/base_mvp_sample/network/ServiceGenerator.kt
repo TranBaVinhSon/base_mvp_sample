3003772971ee9f1eea48f9fde0bdd1954a37c862
Tran B.V. Son <tranbavinhson@gmail.com>
2018-07-24 13:11:36 +0700

    fix square image

--- app/src/main/java/com/example/sontbv/base_mvp_sample/network/ServiceGenerator.kt
+++ app/src/main/java/com/example/sontbv/base_mvp_sample/network/ServiceGenerator.kt
@@ -1,31 +1,39 @@
 package com.example.sontbv.base_mvp_sample.network
 
 import com.example.sontbv.base_mvp_sample.utils.Constants
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.IOException
 
-class ServiceGenerator {
-    // like static in java
-    companion object {
-        var retrofit: Retrofit? = null
-        var httpLoggingInterceptor: HttpLoggingInterceptor? = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        // TODO: Add header here
-        var okHttpClientBuilder: OkHttpClient.Builder? = OkHttpClient.Builder()
-                .addInterceptor(httpLoggingInterceptor)
-        var okHttpClient: OkHttpClient? = okHttpClientBuilder?.build()
 
-        fun <T> createService(serviceClass: Class<T> ): T? {
-            if(retrofit == null){
-                retrofit = Retrofit.Builder()
-                        .client(okHttpClient)
-                        .baseUrl(Constants.BASE_API_URL)
-                        .addConverterFactory(MoshiConverterFactory.create())
-                        .build()
-            }
-            return retrofit?.create(serviceClass)
+object ServiceGenerator {
+    private var retrofit: Retrofit? = null
+    private val httpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val okHttpClientBuilder = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(object: Interceptor{
+                @Throws(IOException::class)
+                override fun intercept(chain: Interceptor.Chain): Response {
+                    val request = chain.request().newBuilder()
+                            .addHeader("Authorization", "Client-ID " + Constants.APPLICATION_ID)
+                            .build()
+                    return chain.proceed(request)
+                }
+            })
+    private val okHttpClient = okHttpClientBuilder.build()
+    fun <T> createService(serviceClass:Class<T>):T? {
+        if (retrofit == null)
+        {
+            retrofit = Retrofit.Builder()
+                    .client(okHttpClient)
+                    .baseUrl(Constants.BASE_API_URL)
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .build()
         }
+        return retrofit?.create(serviceClass)
     }
 }
--- app/src/main/java/com/example/sontbv/base_mvp_sample/widget/SquareImage.kt
+++ app/src/main/java/com/example/sontbv/base_mvp_sample/widget/SquareImage.kt
@@ -1,8 +1,16 @@
 package com.example.sontbv.base_mvp_sample.widget
 
+import android.content.Context
+import android.support.annotation.Nullable
+import android.util.AttributeSet
 import android.widget.ImageView
 
-class SquareImage : ImageView {
-    init {
+class SquareImage:ImageView {
+    constructor(context:Context) : super(context) {}
+    constructor(context:Context, @Nullable attrs:AttributeSet) : super(context, attrs) {}
+    constructor(context: Context, @Nullable attrs: AttributeSet, defStyleAttr:Int) : super(context, attrs, defStyleAttr) {}
+    override fun onMeasure(widthMeasureSpec:Int, heightMeasureSpec:Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth())
     }
 }
