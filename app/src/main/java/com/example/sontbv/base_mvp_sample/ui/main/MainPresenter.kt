d36aa1049b115183b03f887e49d7510ff3c565e1
Truong Nguyen <manhtruonghedspi@gmail.com>
2018-07-26 08:52:25 +0700

    Remove redundant code

--- app/src/main/java/com/example/sontbv/base_mvp_sample/ui/main/MainPresenter.kt
+++ app/src/main/java/com/example/sontbv/base_mvp_sample/ui/main/MainPresenter.kt
@@ -1,50 +1,8 @@
 package com.example.sontbv.base_mvp_sample.ui.main
 
-import com.example.sontbv.base_mvp_sample.data.db.model.Photo
-import com.example.sontbv.base_mvp_sample.network.ApiInterface
-import com.example.sontbv.base_mvp_sample.network.ServiceGenerator
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
 class MainPresenter: MainContract.Presenter {
 
     private lateinit var view: MainContract.View
-
-//    fun onResume() {
-//        view.showProgressBar()
-//        getPhotos()
-//    }
-//
-//    override fun getPhotos() {
-//        var photos: List<Photo>
-//        val apiInterface = ServiceGenerator.createService(ApiInterface::class.java)
-//        val call = apiInterface!!.getPhotos()
-//        call.enqueue(object: Callback<List<Photo>> {
-//            override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
-//                if (response.isSuccessful) {
-//                    photos = response.body()!!
-//                    onPhotosLoaded(photos)
-//                }
-//                else {
-//                }
-//            }
-//            override fun onFailure(call: Call<List<Photo>>, t:Throwable) {
-//
-//            }
-//        })
-//    }
-//
-//    override fun onPhotosLoaded(photos: List<Photo>) {
-//        view.apply {
-//            setPhotos(photos)
-//            hideProgressBar()
-//        }
-//    }
-
-//    fun onDestroy() {
-//        view = null
-//    }
 
     override fun attach(view: MainContract.View) {
         this.view = view
